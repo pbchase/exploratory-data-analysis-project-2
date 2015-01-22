7eb9ff709e04d244603839e4c69daf4a277f4e69
Philip Chase <pbc@ufl.edu>
2015-01-21 21:20:37 -0500

    Add plot3 R code and plot file

---
+++ plot3.R
@@ -0,0 +1,38 @@
+# plot3.R
+# A script to process PM2.5 Emissions Data
+
+# Download data file
+dataFile <- "exdata_data_NEI_data.zip"
+if (!file.exists(dataFile)){
+    print("Downloading data file")
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", dataFile, "curl")
+} else {
+    print("File already exists locally")
+}
+
+# Extract the contents of the zip file
+unzip(dataFile)
+
+# load the pair of data frames
+nei <- readRDS("summarySCC_PM25.rds")
+scc <- readRDS("Source_Classification_Code.rds")
+
+
+# Question:
+# Of the four types of sources indicated by the type (point, nonpoint, onroad,
+# nonroad) variable, which of these four sources have seen decreases in
+# emissions from 1999–2008 for Baltimore City? Which have seen increases in
+# emissions from 1999–2008? Use the ggplot2 plotting system to make a plot
+# answer this question.
+
+library(ggplot2)
+library(dplyr)
+
+baltimore <- subset(nei, fips == "24510")
+groups <- group_by(baltimore, year, type)
+grouped_emissions <- summarize(groups, total_emissions = sum(Emissions))
+
+png(file="plot3.png", bg="transparent", width = 960, height = 480, units = "px",)
+qplot(year, total_emissions, data=grouped_emissions, ylab="Total PM2.5 Emissions",
+    xlab="Year", main= "Annual PM2.5 Emissions in Baltimore by Source", facets = . ~ type)
+dev.off()
